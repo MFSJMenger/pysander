4e3a5e875d0c3fdabf362694dea23c31fb8a5b04
Jason Swails <jason.swails@gmail.com>
2014-09-03 14:46:13 -0400

    Also build a sanderles Python package that links to libsanderles instead of libsander.

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 
 from distutils.core import setup, Extension
 import os
+import shutil
+import sys
 
 amberhome = os.getenv('AMBERHOME')
 
 if amberhome is None:
     raise RuntimeError('AMBERHOME is not set! Cannot compile pysander')
 
-packages = ['sander']
+packages = ['sander', 'sanderles']
+
+shutil.copytree('sander', 'sanderles')
 
 pysander = Extension('sander.pysander',
                      sources=['sander/src/pysandermodule.c'],
                      include_dirs=[os.path.join(amberhome, 'include')],
                      library_dirs=[os.path.join(amberhome, 'lib')],
                      libraries=['sander'])
+pysanderles = Extension('sanderles.pysander',
+                        sources=['sanderles/src/pysandermodule.c'],
+                        include_dirs=[os.path.join(amberhome, 'include')],
+                        library_dirs=[os.path.join(amberhome, 'lib')],
+                        libraries=['sanderles'],
+                        define_macros=[('LES', None)])
 
-setup(name='sander', ext_modules=[pysander], packages=packages)
+setup(name='sanderles', ext_modules=[pysander, pysanderles], packages=packages)
+
+shutil.rmtree('sanderles')
